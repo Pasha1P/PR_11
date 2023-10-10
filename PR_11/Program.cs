971dce5002aed40f8c62a4eb4853b2ab4ac45e7e
Pasha1P <145444858+Pasha1P@users.noreply.github.com>
2023-10-10 20:39:07 +0300

    Add files via upload

---
+++ PR_11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static string ReplaceSumsWithResults(string input)
+    {
+        // Используем регулярное выражение для поиска всех сумм натуральных чисел
+        string pattern = @"(\d+)\+(\d+)";
+        Regex regex = new Regex(pattern);
+
+        // Заменяем найденные суммы на их результаты
+        string result = regex.Replace(input, match =>
+        {
+            int num1 = int.Parse(match.Groups[1].Value);
+            int num2 = int.Parse(match.Groups[2].Value);
+            int sum = num1 + num2;
+            return sum.ToString();
+        });
+
+        return result;
+    }
+
+    static void Main()
+    {
+        Console.Title = " Практическая работа №11";
+        Console.WriteLine("\tЗдравствуйте");
+        Console.WriteLine("Введите строку:");
+        string input = Console.ReadLine();
+        string result = ReplaceSumsWithResults(input);
+
+        Console.WriteLine("Исходная строка: " + input);
+        Console.WriteLine("Результат: " + result);
+        Console.ReadKey();
+    }
+}
